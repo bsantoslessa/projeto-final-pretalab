73f8fdf8416c25d0b160697826cd2b3d06f6a116
bsantoslessa <bsantoslessa@gmail.com>
2022-12-16 18:01:57 -0300

    primeiro commit

---
+++ src/pages/Portfolio.jsx
@@ -0,0 +1,9 @@
+import { Header } from '../components/Header'
+import portfolioImg from '../assets/portfolio.svg'
+export function Portfolio () {
+    return (
+    <>
+    <Header text="Sobre o que eu faço" image={portfolioImg}/>
+    </>
+    )
+}
---
+++ src/pages/Sobre.jsx
@@ -0,0 +1,9 @@
+import { Header } from '../components/Header'
+import sobreImg from '../assets/sobre.svg'
+export function Sobre () {
+    return (
+    <>
+    <Header text="Um pouquinho de mim" image={sobreImg}/>
+    </>
+    )
+}
